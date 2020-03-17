cb2f03a6a3e2d4a55a451e49e2fa13ff953fad82
Julia <juliasommerdania@gmail.com>
2020-03-17 09:53:46 +0100

    Add project files.

---
+++ Console_testing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using BinaryNumbers_Lib;
+using static BinaryNumbers_Lib.Opgave01;
+
+namespace Console_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letter = "Hello world";
+            string hexLetter = ToHex(letter.ToCharArray());
+            hexLetter = letter.StringToHexString();
+
+            Console.WriteLine(hexLetter);
+
+            string transLetter = Translator.HexStringToString(hexLetter);
+
+            Console.WriteLine(transLetter);
+
+
+            Console.ReadKey();
+        }
+    }
+}
